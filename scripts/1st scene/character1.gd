1c479225265ac340672c0fb8763973d7a7f08c4a
Atonbara-Adora <the.atonbara@gmail.com>
2024-08-19 16:22:54 -0400

    first scene

--- scripts/1st scene/character1.gd
+++ scripts/1st scene/character1.gd
@@ -2,7 +2,7 @@ extends Sprite2D
 
 var mouse_in = false
 
-#var hover_character = preload("")
+var hover_character = preload("res://art/character art/RedDeadWOutline.webp")
 var regular_character = preload("res://art/character art/reddead.png")
 
 # Called when the node enters the scene tree for the first time.
@@ -15,13 +15,18 @@ func _process(delta):
 	pass
 
 func _input(event):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT and mouse_in:
 		if get_rect().has_point(to_local(event.position)):
+			self.visible = false
+			# Optionally, you can disable processing or input if needed
+			self.set_process(false)
+			self.set_process_input(false)
+			# Now, change to the next scene
 			get_tree().change_scene_to_file("res://enemy scenes/red_dead.tscn")
 
 func _on_area_2d_mouse_entered():
 	mouse_in = true
-	#texture = hover_character
+	texture = hover_character
 
 
 func _on_area_2d_area_exited(area):
