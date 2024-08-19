023b27d8a56066d0aa587912c61138ce5ce23406
Atonbara-Adora <the.atonbara@gmail.com>
2024-08-19 17:34:28 -0400

    Basic intro scene and dialogue manager

--- scripts/1st scene/bowser.gd
+++ scripts/1st scene/bowser.gd
@@ -1,8 +1,8 @@
 extends Sprite2D
 
 var mouse_in = false
-var hover_character = preload("res://art/character art/RedDeadWOutline.webp")
-var regular_character = preload("res://art/character art/reddead.png")
+var hover_character = preload("res://art/character art/SentryFlashStillOutline.webp")
+var regular_character = preload("res://art/character art/robot.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,15 +13,15 @@ func _ready():
 func _process(delta):
 	pass
 
-
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT and mouse_in:
 		if get_rect().has_point(to_local(event.position)):
-			get_tree().change_scene_to_file("res://RedDeadBattle.tscn")
-
+			get_tree().change_scene_to_file("res://RobotBattle.tscn")
+			
 func _on_area_2d_mouse_entered():
 	mouse_in = true
 	texture = hover_character
+
 
 func _on_area_2d_mouse_exited():
 	mouse_in = false
