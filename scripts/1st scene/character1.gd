5b78f214002e12bb172066b3e9d2572d6e052d52
Atonbara-Adora <the.atonbara@gmail.com>
2024-08-19 01:29:34 -0400

    Bedroom
    bedroom scene with one clickable character

---
+++ scripts/1st scene/character1.gd
@@ -0,0 +1,29 @@
+extends Sprite2D
+
+var mouse_in = false
+
+#var hover_character = preload("")
+var regular_character = preload("res://art/character art/reddead.png")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if get_rect().has_point(to_local(event.position)):
+			get_tree().change_scene_to_file("res://enemy scenes/red_dead.tscn")
+
+func _on_area_2d_mouse_entered():
+	mouse_in = true
+	#texture = hover_character
+
+
+func _on_area_2d_area_exited(area):
+	mouse_in = false
+	texture = regular_character
