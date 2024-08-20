9fed635b302ff89390d2c515f427c0940bd14e39
DarioMeile <dancing.giants.studio@gmail.com>
2024-08-20 06:08:15 -0700

    more changes

--- Scenes/start.gd
+++ Scenes/start.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+
+@export var array_all_dialogues_scene_1: Array[String]
+@export var array_all_dialogues_scene_2: Array[String]
+
 @onready var start_button = %Start
 @onready var option_button = %Options
 @onready var black_screen = %Black_Screen
@@ -11,6 +15,12 @@ extends Node2D
 @onready var audio_node = $Audio
 @onready var music_i = %Music_InitialLoop
 @onready var music_c = %Music_ContinuousLoop
+@onready var sfx_hover = %SFX_Hover
+@onready var sfx_accept = %SFX_Accept
+@onready var sfx_back = %SFX_Back
+
+@onready var current_scene = 0
+@onready var entered: bool = false
 
 func _ready() -> void:
 	black_screen.show()
@@ -29,8 +39,27 @@ func _ready() -> void:
 	await _tween2.finished
 	black_screen.hide()
 
+func _process(delta: float) -> void:
+	match current_scene:
+		0:
+			pass
+		1:
+			if Input.is_action_just_pressed("ui_accept"):
+				current_scene = 2
+				entered = false
+				return
+			if !entered:
+				_start_first_dialogue()
+			entered = true
+		2:
+			pass
+		3:
+			pass
+		4:
+			pass
 
 func _on_start_pressed() -> void:
+	sfx_accept.play()
 	var _tween = create_tween()
 	_tween.set_parallel(true)
 	_tween.tween_property(gray_panel, "self_modulate", Color(1,1,1,0), 0.3).from_current()
@@ -39,7 +68,23 @@ func _on_start_pressed() -> void:
 	await _tween.finished
 	button_container.hide()
 	title.hide()
+	await _tween.finished
+	await get_tree().create_timer(1).timeout
+	current_scene = 1
 
 
 func _on_music_initial_loop_finished() -> void:
 	music_c.playing = true
+
+
+func _on_start_mouse_entered() -> void:
+	sfx_hover.play()
+
+
+func _on_options_mouse_entered() -> void:
+	sfx_hover.play()
+
+func _start_first_dialogue():
+	var _array_options: int = 0
+	
+	pass
---
+++ scripts/UI Elements/speech_bubble_in_overworld.gd
@@ -0,0 +1,37 @@
+extends Control
+
+@onready var speech_label = $MarginContainer/Expression
+var transitioning = false
+
+func _ready():
+	_appear()
+
+func _initialize(_text):
+	speech_label = $MarginContainer/Expression
+	speech_label.text = _text
+
+func _appear():
+	position.y = position.y + 20
+	show()
+	var _tween = create_tween()
+	_tween.set_ease(Tween.EASE_IN)
+	_tween.set_trans(Tween.TRANS_LINEAR)
+	_tween.set_parallel(true)
+	_tween.tween_property(self, "modulate", Color(Color.WHITE,1), 0.25).from(Color(Color.WHITE,0.0))
+	_tween.tween_property(self, "position", Vector2(position.x, position.y - 20), 0.25).from_current()
+	await _tween.finished
+
+func _dissappear():
+	if transitioning:
+		return
+	transitioning = true
+
+	await get_tree().create_timer(2).timeout
+	
+	var _tween = create_tween()
+	_tween.set_ease(Tween.EASE_IN)
+	_tween.set_trans(Tween.TRANS_LINEAR)
+	_tween.tween_property(self, "modulate", Color(Color.WHITE,0.0), 0.1).from(Color(Color.WHITE,1.0))
+	
+	await _tween.finished
+	queue_free()
