c26b36251208e66e1a033abb8eb9731e7fe2a3f7
DarioMeile <dancing.giants.studio@gmail.com>
2024-08-19 21:07:06 -0700

    Fixed a broken dependency in boxer battle

--- scripts/player_body.gd
+++ scripts/player_body.gd
@@ -1,7 +1,7 @@
 extends Body
 @export var all_profiles: Array[Body_Part] = [preload("res://resources/bodyparts/base/base_torso.tres"), preload("res://resources/bodyparts/base/base_head.tres"), preload("res://resources/bodyparts/base/base_larm.tres"), preload("res://resources/bodyparts/base/base_rarm.tres"), preload("res://resources/bodyparts/base/base_legs.tres"),
 preload("res://resources/bodyparts/boxer/boxer_torso.tres"), preload("res://resources/bodyparts/boxer/boxer_head.tres"), preload("res://resources/bodyparts/boxer/boxer_larm.tres"), preload("res://resources/bodyparts/boxer/boxer_rarm.tres"), preload("res://resources/bodyparts/boxer/boxer_legs.tres"),
-preload("res://resources/bodyparts/reddead/red_torso.tres"),preload("res://resources/bodyparts/reddead/red_head.tres"), preload("res://resources/bodyparts/reddead/red_larm.tres"), preload("res://resources/bodyparts/reddead/red_rarm.tres"), preload("res://resources/bodyparts/reddead/red_legs.tres"),
+preload("res://resources/bodyparts/red/red_torso.tres"),preload("res://resources/bodyparts/red/red_head.tres"), preload("res://resources/bodyparts/red/red_larm.tres"), preload("res://resources/bodyparts/red/red_rarm.tres"), preload("res://resources/bodyparts/red/red_legs.tres"),
 preload("res://resources/bodyparts/sentryflash/sentry_torso.tres"), preload("res://resources/bodyparts/sentryflash/sentry_head.tres"), preload("res://resources/bodyparts/sentryflash/sentry_larm.tres"), preload("res://resources/bodyparts/sentryflash/sentry_rarm.tres"), preload("res://resources/bodyparts/sentryflash/sentry_legs.tres"),
 preload("res://resources/bodyparts/sparkstriker/spark_torso.tres"), preload("res://resources/bodyparts/sparkstriker/spark_head.tres"), preload("res://resources/bodyparts/sparkstriker/spark_larm.tres"), preload("res://resources/bodyparts/sparkstriker/spark_rarm.tres"), preload("res://resources/bodyparts/sparkstriker/spark_legs.tres")]
 @export var all_textures: Array[Resource] = [preload("res://art/character art/Individual Pieces/MODBase.png"), preload("res://art/character art/Individual Pieces/MODBoxer.png"), preload("res://art/character art/Individual Pieces/MODRedDead.png"), preload("res://art/character art/Individual Pieces/MODSentryFlash.png"), preload("res://art/character art/Individual Pieces/MODSparkStriker.png")]
@@ -27,10 +27,14 @@ func _ready() -> void:
 	current_body_parts.append(all_profiles[2 +  (5 * Global_Player_Information.character_body_parts["Left Arm"])])
 	current_body_parts.append(all_profiles[3 +  (5 * Global_Player_Information.character_body_parts["Right Arm"])])
 	current_body_parts.append(all_profiles[4 +  (5 * Global_Player_Information.character_body_parts["Legs"])])
+
 	_update_appearance()
 
 
 func _update_appearance():
+	
+	
+	
 	for n in current_body_parts.size():
 		var _location_in_array = all_profiles[current_body_parts[n].body_part_id + (5 * current_body_parts[n].character_base)]
 		var _bodypart_sprite = null
